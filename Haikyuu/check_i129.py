765661b6bfa877f16d3e3f940b7486aa13aa08e6
Praneeth <sk.d.ai.9956@gmail.com>
2021-02-12 17:11:05 +0530

    first commit

---
+++ Haikyuu/check_i129.py
@@ -0,0 +1,16 @@
+import json
+from collections import Counter
+from pprint import pprint
+
+with open("i129.json") as f:
+    data  = json.load(f)
+
+result_list = []
+for keys,values in data.items():
+    result_list.extend(list(values.keys()))
+
+
+a = Counter(result_list)
+pprint(a)
+
+
